5cde37c70ef5f69a5737745814fafac8cd3d9aab
HuILeN71 <88256467+HuILeN71@users.noreply.github.com>
2025-01-21 21:49:56 +0100

    Tracking game state

--- src/main/java/nl/dacolina/fetchranalytics/FetchrAnalytics.java
+++ src/main/java/nl/dacolina/fetchranalytics/FetchrAnalytics.java
@@ -2,7 +2,9 @@ package nl.dacolina.fetchranalytics;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.MinecraftServer;
+import nl.dacolina.fetchranalytics.managers.GameManager;
 import nl.dacolina.fetchranalytics.onstartup.AfterStartUp;
 import nl.dacolina.fetchranalytics.onstartup.OnStartUp;
 import org.slf4j.Logger;
@@ -10,8 +12,8 @@ import org.slf4j.LoggerFactory;
 
 public class FetchrAnalytics implements ModInitializer {
 	public static final String MOD_ID = "fetchranalytics";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	private GameManager bingoManager;
 
 	@Override
 	public void onInitialize() {
@@ -26,7 +28,20 @@ public class FetchrAnalytics implements ModInitializer {
 	}
 
 	private void onServerStarted(MinecraftServer server) {
-		FetchrAnalytics.LOGGER.info("Running after start-up checks!");
+		// Start game checks, see if database is ready to receive data.
+		FetchrAnalytics.LOGGER.info("Running after start-up checks...");
 		AfterStartUp.afterStartUp(server);
+		FetchrAnalytics.LOGGER.info("Finished start-up checks!");
+
+		// Start a GameManager Class to manage all the games
+		bingoManager = new GameManager(server);
+
+		// Register event where something is to be checked every tick
+		ServerTickEvents.START_SERVER_TICK.register(this::onServerTick);
+
+	}
+
+	private void onServerTick(MinecraftServer server) {
+		bingoManager.tick();
 	}
 }
--- src/main/java/nl/dacolina/fetchranalytics/managers/GameManager.java
+++ src/main/java/nl/dacolina/fetchranalytics/managers/GameManager.java
@@ -1,4 +1,46 @@
 package nl.dacolina.fetchranalytics.managers;
 
+import net.minecraft.scoreboard.ReadableScoreboardScore;
+import net.minecraft.scoreboard.ScoreHolder;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.server.MinecraftServer;
+import nl.dacolina.fetchranalytics.FetchrAnalytics;
+
 public class GameManager {
+
+    private MinecraftServer server;
+
+    public GameManager(MinecraftServer server) {
+        this.server = server;
+    }
+
+    public void tick() {
+        FetchrAnalytics.LOGGER.info("Current game state: " + getGameState());
+    }
+
+    private int getGameState () {
+        // Data from mod suggests: he current game stat 0 = Lobby / Not in game; 1 = Starting / chunk pre-gen; 2 = Skybox phase; 3 = Game started
+        return getStateFromServerInstance(server);
+    }
+
+    private static int getStateFromServerInstance(MinecraftServer server) {
+        Scoreboard scoreboard = server.getScoreboard();
+
+        ScoreboardObjective objective = scoreboard.getNullableObjective("fetchr.state");
+
+        if(objective != null) {
+            ScoreHolder gameStatePlayer = ScoreHolder.fromName("$game_state"); // The special "player" name in the scoreboard
+
+            ReadableScoreboardScore gameStateScore = scoreboard.getScore(gameStatePlayer, objective);
+
+            // Get the score value\
+            int gameStateValue = gameStateScore.getScore();
+            return gameStateValue;
+        }
+
+        return -1;
+
+    }
+
 }
